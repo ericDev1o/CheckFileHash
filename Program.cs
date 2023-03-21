0963ff07a88a68158bfa4d1b34249bdc7f0fcff5
Eric Faraut <eric.faraut@hotmail.fr>
2023-03-21 10:18:50 +0100

    SDK released March 14th update is done.

--- Program.cs
+++ Program.cs
@@ -8,11 +8,11 @@ namespace SHA512test
         static void Main(string[] args)
         {
             HashAlgorithm sHA512 = SHA512.Create();
-            using(FileStream fs = new FileStream(@"C:\softwareDownloads\dotnet-sdk-7.0.200-win-x64.exe",FileMode.Open))
+            using(FileStream fs = new FileStream(@"C:\softwareDownloads\dotnet-sdk-7.0.202-win-x64.exe",FileMode.Open))
             {
                 byte[] hashComputed = sHA512.ComputeHash(fs);
                 string hashComputedString = BitConverter.ToString(hashComputed).ToLower().Replace("-","");
-                string hashExpectedString = "e41ce0c55181b564b29b3e4bd9e76be294db57d2ec0f152a295bdbae37b422a46bdbcb1c680f2d56d098ac8298533658d3d4ff8c50f1716652ebd012e93da292";
+                string hashExpectedString = "58e9437f2ead1e9edd67d837f7d2e1313d866d510629465b0485965a9540c65eddd6fffafad9cf3261cb476a1dfff20404908f3bf06a7e01bc3bebc0bcb27d39";
                 if(hashComputedString==hashExpectedString)
                     Console.WriteLine("Hash checked OK: computed "+hashComputedString+" = expected "+hashExpectedString);
                 else
