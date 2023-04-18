160e18ff9f917dd94e2803f6e0ad1afdb4cd687e
Eric Faraut <eric.faraut@hotmail.fr>
2023-04-18 23:09:34 +0200

    SDK 7.0.203 update: 	modified:   ControlHandler/CheckSHA.cs 	modified:   MainWindow.xaml 	deleted:    Program.cs

--- ControlHandler/CheckSHA.cs
+++ ControlHandler/CheckSHA.cs
@@ -24,11 +24,11 @@ public class CheckSHA
     public static ValueTuple<string, string> Check(string path, string expectedHash)
     {
         HashAlgorithm sHA512 = SHA512.Create();
-        using(FileStream fs = new FileStream(@"C:\softwareDownloads\dotnet-sdk-7.0.202-win-x64.exe",FileMode.Open))
+        using(FileStream fs = new FileStream(@"C:\softwareDownloads\dotnet-sdk-7.0.203-win-x64.exe",FileMode.Open))
         {
             byte[] hashComputed = sHA512.ComputeHash(fs);
             string hashComputedString = BitConverter.ToString(hashComputed).ToLower().Replace("-","");
-            string hashExpectedString = "58e9437f2ead1e9edd67d837f7d2e1313d866d510629465b0485965a9540c65eddd6fffafad9cf3261cb476a1dfff20404908f3bf06a7e01bc3bebc0bcb27d39";
+            string hashExpectedString = "b2c09ba8454da835304075760c4f355ee1e394c47402800695c75e2da964c5dcd0bb70c1f0c5c67c425369d7fdc9eb060a3936ea755a140342ba12ea47d7cd8b";
             ValueTuple<string, string> checkResult =("", "");
             if(hashComputedString==hashExpectedString)
                 checkResult = 
